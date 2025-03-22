3268a809007f9361220cdd255bf29ff98c604f40
Khang <metchetme123@gmail.com>
2025-03-22 19:39:28 +0700

    phe duyet phu luc

--- src/pages/appendix/staff/AppendixManagement.jsx
+++ src/pages/appendix/staff/AppendixManagement.jsx
@@ -111,7 +111,7 @@ const AppendixManagement = () => {
             dataIndex: "title",
             key: "title",
             sorter: (a, b) => a.title.localeCompare(b.title),
-            render: (text) => (
+            render: (text,record) => (
                 <Link to={`${user.roles[0] === "ROLE_STAFF" ? `/appendixDetail/${record.contractId}/${record.addendumType.addendumTypeId}` : `/manager/appendixDetail/${record.contractId}/${record.addendumType.addendumTypeId}`}`} className="font-bold text-[#228eff] cursor-pointer">
                 <p> {text} </p>
                  </Link>
